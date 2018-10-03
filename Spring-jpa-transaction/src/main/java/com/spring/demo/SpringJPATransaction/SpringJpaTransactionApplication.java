ef2175f503036436b5b55bd8a1bee84a9ab41d61
SJ00495527 <joshiashish296@gmail.com>
2018-10-03 16:02:16 +0530

    init default project structure changes

---
+++ Spring-jpa-transaction/src/main/java/com/spring/demo/SpringJPATransaction/SpringJpaTransactionApplication.java
@@ -0,0 +1,12 @@
+package com.spring.demo.SpringJPATransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaTransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaTransactionApplication.class, args);
+	}
+}
